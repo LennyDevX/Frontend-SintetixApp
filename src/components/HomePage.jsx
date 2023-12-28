632c56b3eadf22bd8f2a1f0c0b12a50b6279de92
lennymercadovaldez <71667982+lennymercadovaldez@users.noreply.github.com>
2023-12-28 16:49:00 -0500

    Delete unused images and update routes and components.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,6 +4,10 @@ import { DataTeamCards } from '../data/DataTeamCard';
 import NavBar from './NavBar'; 
 import RegisterForm from './RegisterForm'; 
 import LoginForm from './LoginForm';
+import Carousel from './Carousel';
+import Image1 from '../../Img/conectando.gif';
+import Image2 from '../../Img/conectando2.gif';
+import image3 from '../../Img/conectando4.gif';
 
 function HomePage() {
     const [showLogin, setShowLogin] = useState(false);
@@ -20,10 +24,16 @@ function HomePage() {
     return (
         <div>
             <NavBar onLoginClick={onLoginClick} onRegisterClick={onRegisterClick} />
+
             <section className="gridcard">
+            
+            <Carousel imageSrc={Image1} altText="Primera imagen" />
+            <Carousel imageSrc={Image2} altText="Segunda imagen" />
+            <Carousel imageSrc={image3} altText="Tercera imagen" />
                 {DataTeamCards.map((team) => ( 
                     <TeamCards
-                        key={team.title} // Cambiado a un valor único
+                        key={team.index}
+                        // Cambiado a un valor único
                         title={team.title}
                         description={team.description}
                         link={team.link}
--- src/data/DataTeamCard.js
+++ src/data/DataTeamCard.js
@@ -2,24 +2,27 @@ import SintetixLogo from '../../Img/SintetixLogo.png'
 import ReactLogo from '../../Img/ReactLogo.png'
 import DiscordLogo from '../../Img/DiscordLogo.png'
 import NodeLogo from '../../Img/NodeLogo.png'
+import Copilot from '../../Img/CopilotLogo.png'
+import BootstrapLogo from '../../Img/BootstrapLogo.png'
 
 export const DataTeamCards = [
   {
-    title: "Sintetix Hub",
-    description: "Somos una organizacion Open Source que desarrolla software y herramientas para la comunidad de Sintetix, creemos en el trabajo colectivo, la comunidad levanta los mejores proyectos segun vayan surgiendo diferentes necesidades. WebApp, IA, Bots, etc.",
+    title: " 💻 Sintetix Hub 💻",
+    description: "Somos una organizacion Open Source que desarrolla software y herramientas para la comunidad de Sintetix, creemos en el trabajo colectivo, la comunidad levanta los mejores proyectos segun vayan surgiendo diferentes necesidades. WebApp, IA, Bots, etc. Construimos desde 0 y 100% personalizado a los ajsutes de cualquier cliente que dese adquirir nuestros futuros servicios de desarrllo",
     ButtonText: "Ver más",
     link: "#",
     logo: SintetixLogo,
   },
   {
-    description: "Estamos siempre en la busqueda continua de nuevos talentos, tenemos una comunidad en Discord donde nos reunimos y compartimos ideas, si quieres colaborar con nosotros, unete a nuestro Discord y cuentanos tu idea, o si quieres ayudar en algun proyecto, tambien puedes hacerlo.",
+    title: "🌐 Comunidad 🌐",
+    description: "Estamos siempre en la busqueda continua de nuevos talentos, tenemos una comunidad en Discord donde nos reunimos y compartimos ideas, si quieres colaborar con nosotros, unete a nuestro Discord y cuentanos tu idea, o si quieres ayudar en algun proyecto, tambien puedes hacerlo. Somos una comunidad diversa, cualquiera que sea tu especialidad tenemos un espacio para ti y tu creatividad.",
     ButtonText : "Unirse a Discord",
     link: "https://discord.gg/uM8wTs4x",
     logo: DiscordLogo,
 
   }, 
   {
-    title: "We ♥️ React",
+    title: "We 💙 React",
     description: "Bajo el capoot de esta web esta Reactt, una libreria de JavaScript para construir potentes y visuales interfaces de usuario, React es mantenido por Facebook y una comunidad de desarrolladores individuales, y empresas que colaboran activamente en el desarrolo. Perfectamente integrable con otras librerias o frameworks como Node.js, Express, etc.",
     ButtonText : "Saber mas",
     link: "https://es.reactjs.org/",
@@ -27,10 +30,24 @@ export const DataTeamCards = [
 
   },
   {
-    title: "Node & Express 🚀",
-    description: "para el backend decidimos usar express, un framework de Node.js, que nos permite crear aplicaciones web y API de una manera muy sencilla, ademas de ser muy rapido y escalable. Es perfecto para aplicaciones web modernas, con grandes capacidades de procesamiento y caracteristicas de vanguardia como WebSockets y streaming.",
+    title: "💚 NodeJS & Express 🚀",
+    description: "para el backend decidimos usar express, un framework de Node.js, que nos permite crear aplicaciones web y API de una manera muy sencilla, ademas de ser muy rapido y escalable. Es perfecto para aplicaciones web modernas, con grandes capacidades de procesamiento y caracteristicas de vanguardia como WebSockets y streaming. Incluye conmpoentes de seguridad, manejo de errores, optimizaciones, etc.",
     link: "https://www.google.com",
     ButtonText : "Saber mas",
     logo: NodeLogo,
+  },
+  {
+    title: " Con Copilot es mejor 🤖",
+    description: "La IA puede sorprendernos con lo eficiente que nos permite hacer las cosas, por eso es importante conocer las posibilidades que tenemos con herramientas de esti tipo. Sinteteix usa IA para el desarrollo de sus productos/servicios. Creando increibles Apps, herramientas y bots para la comunidad. Si quieres saber mas sobre IA, unete a nuestra comunidad en Discord y pregunta por nuestros expertos.",
+    link: "https://www.google.com",
+    ButtonText : "Saber mas",
+    logo: Copilot,
+  },
+  {
+    title: "💜 Boostrap 🎨",
+    description: "Usamos React-Bootstrpa para los estilos de esta web, es una libreria de componentes de interfaz de usuario de código abierto para React, que nos permite crear interfaces de usuario de aspecto profesional y con un diseño muy atractivo, ademas de ser muy facil de usar y personalizar. Viene con componentes listos para usar, que nos permiten crear paginas web de una manera muy sencilla",
+    link: "https://www.google.com",
+    ButtonText : "Saber mas",
+    logo: BootstrapLogo,
   },
 ]
